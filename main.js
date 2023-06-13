57fff69cd62aa8e48bbe6e33814b73499f6aa9d2
Alanssrv <alanssrv@gmail.com>
2023-06-12 21:48:37 -0300

    att iteracao da lista e adicao de validacoes

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+    const elemento = document.querySelector(idElementoAudio);
+
+    if (elemento && elemento.localName === 'audio')
+        elemento.play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-while (contador < listaDeTeclas.length) {
-    const tecla = listaDeTeclas[contador];
+listaDeTeclas.forEach(tecla => {
     const instrumento = tecla.classList[1];
 
     const idAudio = `#som_${instrumento}`;
@@ -14,5 +15,13 @@ while (contador < listaDeTeclas.length) {
     tecla.onclick = function () {
         tocaSom(idAudio);
     };
-    contador++;
-}
+
+    tecla.onkeydown = function (e) {
+        if(e.which === 13 || e.which === 32)
+            tecla.classList.add('ativa');
+    };
+
+    tecla.onkeyup = function (e) {
+        tecla.classList.remove('ativa');
+    };
+});
